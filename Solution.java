ec6d5144f871f717073c2aa476ce02b7e3bf6bc5
Harper44L <71484775+Harper44L@users.noreply.github.com>
2021-06-19 23:36:25 +0100

    旋转数组
    给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
    
    输入: nums = [1,2,3,4,5,6,7], k = 3
    输出: [5,6,7,1,2,3,4]

--- Solution.java
+++ Solution.java
@@ -1,51 +1,42 @@
 package com.Harper;
-
-public class Solution {
-    int count=0;
-    public int removeDuplicates(int[] nums) {
-
-        if(nums.length==0){
-            return 0;
-        }
-
-        for(int i=1;i< nums.length;i++){
-            if(nums[i]!=nums[count]){
-                count++;
-                nums[count]=nums[i];
-            }
-        }
-
-
-        for (int i = 0; i <=count; i++) {
-            System.out.println(nums[i]);
-        }
-        return count+1;
-    }
-}
-
+// 我的解法：不是原地
 //class Solution {
-//    public int removeDuplicates(int[] nums) {
-//        int n = nums.length;
-//        if (n == 0) {
-//            return 0;
-//        }
-//        int fast = 1, slow = 1;
-//        while (fast < n) {
-//            if (nums[fast] != nums[fast - 1]) {
-//                nums[slow] = nums[fast];
-//                ++slow;
-//            }
-//            ++fast;
+//    public void rotate(int[] nums, int k) {
+//        int[] arr = new int[nums.length];
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[(i+k)%nums.length]=nums[i];
 //        }
 //
-//        for (int i = 0; i < slow; i++) {
-//            System.out.println(nums[i]);
-//
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.print(arr[i]);
 //        }
-//        return slow;
 //    }
 //}
 
 
+// 原地解法！！！超牛
+class Solution {
+    public void rotate(int[] nums, int k) {
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
 
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]);
+        }
+    }
+
+    public void reverse(int[] arr,int start,int end){
+
+        int temp=0;
+        while(start<end){
+            temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+}
 
